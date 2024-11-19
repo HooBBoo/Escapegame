188dd0d58216f50192f53622883cc41d482ea1f0
kjm0417 <ekdh41779@gmail.com>
2024-11-19 18:56:48 +0900

    [Update] UITestScene 업데이트

--- Assets/Scripts/Manager/TriggerManager.cs
+++ Assets/Scripts/Manager/TriggerManager.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+public enum TriggerType
+{
+    FrontTrigger,
+    BackTrigger,
+    RandomMapTrigger,
+    // 필요한 만큼 더 추가 가능
+}
+
 public class TriggerManager : Singleton<TriggerManager>
 {
     //일단 정답 트리거 부터 생각... 정답 트리거는 만약에 맵에 바뀐 오브젝트랑 기믹이 없다면 fronttrigger로 가야 통과 만약 반대로 가면 0으로 리셋
@@ -7,7 +15,11 @@ public class TriggerManager : Singleton<TriggerManager>
     
     //오브젝트를 램덤으로 기믹을 보여줘야하는데 만약에 맵 랜덤 트리거에 충돌을 했다면, 그 트리거는 false해주고 
     
-    public GameObject[] TriggerObjects; //trigger를 받아온다.(앞에 트리거, 뒤에 트리거, 맵 변경 트리거)
-    
+    public GameObject[] triggerObjects; //trigger를 받아온다.(앞에 트리거, 뒤에 트리거, 맵 변경 트리거)
+    public TriggerType triggerType;
+    public void ChangeStage()
+    {
+        //triggerObjects
+    }
     
 }
